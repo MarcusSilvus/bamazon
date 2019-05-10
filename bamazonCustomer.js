eb557f6f7e19075beba74dd82955fc6a6fcf47b0
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-05-09 20:49:16 -0700

    created connect between node.js and bamazonDB

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,2 +1,11 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+
+var connection  = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'squackBox1!',
+  database: 'bamazonDB'
+});
+
