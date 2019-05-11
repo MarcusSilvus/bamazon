42cb93dc4a5f989f776997c85b4b33f5d3df216f
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-05-11 16:34:24 -0700

    exit function built and working and building the order item function

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
-var connection  = mysql.createConnection({
+var connection = mysql.createConnection({
   host: 'localhost',
   port: '3306',
   user: 'root',
@@ -9,41 +9,87 @@ var connection  = mysql.createConnection({
   database: 'bamazomDB'
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   openShop();
 });
 
 // function that displays welcome and items
 function openShop() {
-  console.log('\n                           * WELCOME *');
-  console.log('*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*');
-  console.log('|                            Bamazon                        |');
-  console.log('|                       Climbing Department                 |');
-  console.log('*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*');
+  console.log('\n                                        * WELCOME *');
+  console.log('*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*');
+  console.log('|                                         Bamazon                                     |');
+  console.log('|                                   Climbing Department                               |');
+  console.log('*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*');
+  console.log('\n');
+  console.log(
+    '|Item Number',
+    '   |Product',
+    '                  |Deparment',
+    '                  |Price',
+    '          |Stock'
+  );
 
-  connection.query("SELECT * FROM products", function(err, res) {
+
+  connection.query("SELECT * FROM products", function (err, res) {
     if (err) throw err;
     // console.log(res);
     for (var i = 0; i < res.length; i++) {
       console.log(
-        res[i].item_id, 
-        res[i].product_name, 
-        res[i].department_name,
-        res[i].price,
-        res[i].stock,
-        );
+        "    " + res[i].item_id,
+        "           " + res[i].product_name,
+        "                 " + res[i].department_name,
+        "               " + res[i].price,
+        "               " + res[i].stock
+      );
     }
-    
+    console.log('\n');
+    shopping();
   });
-  
-  connection.end();
+}
+
+// function to ask shopper what item_id they want
+function shopping() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "How can we help you?",
+      choices: [
+        "Purchase gear",
+        "Exit"
+      ]
+    })
+    .then(function (answer) {
+      switch (answer.action) {
+        case "Purchase gear":
+          buyGear();
+          break;
+
+        case "Exit":
+          console.log('\n\n      Thanks for shopping with us!');
+          console.log('         Come back any time!');        
+          console.log('\n\n'); 
+          connection.end();
+          break;
+      }
+    });
+}
+
+// ask shopper which item they want
+function buyGear() {
+  inquirer
+    .prompt({
+      name: "gear",
+      type: "input",
+      message: "Please enter item number"
+    })
+    .then(function(){
+    // ask shopper how many of that item they want
+    });
 }
 
 
-// ask shopper what item_id they want
-
-// ask shopper how many of that item they want
 
 //if there is not enough of that item, tell the shopper so
 // and direct them back to the original prompt
