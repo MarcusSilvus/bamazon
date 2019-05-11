3aad8953ab95ef2d15cfdee427b82bc2920a6241
Mark Silva <marksilva@Marks-MacBook-Pro.local>
2019-05-10 19:22:24 -0700

    psuedo code for the functionality of the app

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -9,3 +9,20 @@ var connection  = mysql.createConnection({
   database: 'bamazonDB'
 });
 
+connection.connect(function(err) {
+  if (err) throw err;
+  openShop();
+});
+
+// when program is run, display welcome and items
+
+// ask shopper what item_id they want
+
+// ask shopper how many of that item they want
+
+//if there is not enough of that item, tell the shopper so
+// and direct them back to the original prompt
+
+//if we have the amount in stock, the stock number will update
+// and the total will display
+
